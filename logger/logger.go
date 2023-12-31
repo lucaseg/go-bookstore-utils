ad13861fff05325cb39cab08155c5aa3ea799b7f
Lucas Gonzalez <lucasezequielgo@gmail.com>
2023-12-31 17:15:04 -0300

    update error utils

---
+++ logger/logger.go
@@ -0,0 +1,69 @@
+package logger
+
+import (
+	"fmt"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+const (
+	envLogLevel = "LOG_LEVEL"
+)
+
+var (
+	log logger
+)
+
+type BookStoreLogger interface {
+	Printf(format string, v ...interface{})
+}
+
+type logger struct {
+	log *zap.Logger
+}
+
+func init() {
+	logConfig := zap.Config{
+		OutputPaths: []string{"stdout"},
+		Level:       zap.NewAtomicLevelAt(zap.InfoLevel),
+		Encoding:    "json",
+		EncoderConfig: zapcore.EncoderConfig{
+			LevelKey:     "json",
+			TimeKey:      "time",
+			MessageKey:   "msg",
+			EncodeTime:   zapcore.ISO8601TimeEncoder,
+			EncodeLevel:  zapcore.LowercaseLevelEncoder,
+			EncodeCaller: zapcore.ShortCallerEncoder,
+		},
+	}
+
+	var err error
+	if log.log, err = logConfig.Build(); err != nil {
+		panic(err)
+	}
+}
+
+func GetLogger() BookStoreLogger {
+	return log
+}
+
+func (l logger) Printf(format string, v ...interface{}) {
+	if len(v) == 0 {
+		Info(format)
+	} else {
+		Info(fmt.Sprintf(format, v...))
+	}
+}
+
+func Info(msg string, tags ...zap.Field) {
+	log.log.Info(msg, tags...)
+	_ = log.log.Sync()
+}
+
+func Error(msg string, err error, tags ...zap.Field) {
+	if err != nil {
+		tags = append(tags, zap.NamedError("error", err))
+	}
+	log.log.Error(msg, tags...)
+	_ = log.log.Sync()
+}
--- rest_errors/rest_errors.go
+++ rest_errors/rest_errors.go
@@ -1,33 +1,68 @@
 package rest_errors
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
-type RestError struct {
-	Status  int
-	Message string
-	Error   string
+type RestError interface {
+	Status() int
+	Message() string
+	Error() string
+	Causes() []interface{}
 }
 
-func BadRequest(message string) *RestError {
-	return &RestError{
-		http.StatusBadRequest,
-		message,
-		"bad_request",
+type restError struct {
+	status  int           `json:"status"`
+	message string        `json:"message"`
+	error   string        `json:"error"`
+	causes  []interface{} `json:"causes"`
+}
+
+func (e restError) Status() int {
+	return e.status
+}
+func (e restError) Message() string {
+	return e.message
+}
+
+func (e restError) Causes() []interface{} {
+	return e.causes
+}
+
+func (e restError) Error() string {
+	return fmt.Sprintf("message: %s - status: %d - error: %s - causes: [ %v ]", e.message, e.status, e.error, e.causes)
+}
+
+func NewRestError(status int, message string, error string, causes []interface{}) RestError {
+	return restError{
+		status:  status,
+		message: message,
+		error:   error,
+		causes:  causes,
 	}
 }
 
-func NotFound(message string) *RestError {
-	return &RestError{
-		http.StatusNotFound,
-		message,
-		"not_found",
+func BadRequest(message string) RestError {
+	return restError{
+		status:  http.StatusBadRequest,
+		message: message,
+		error:   "bad_request",
 	}
 }
 
-func InteralServerError(message string) *RestError {
-	return &RestError{
-		http.StatusInternalServerError,
-		message,
-		"internal_server_error",
+func NotFound(message string) RestError {
+	return &restError{
+		status:  http.StatusNotFound,
+		message: message,
+		error:   "not_found",
+	}
+}
+
+func InternalServerError(message string) RestError {
+	return &restError{
+		status:  http.StatusInternalServerError,
+		message: message,
+		error:   "internal_server_error",
 	}
 }
